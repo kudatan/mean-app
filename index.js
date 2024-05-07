2bded4afb8766b6c0933db8bdc8757da9d796454
kudatan <webkuda@gmail.com>
2024-05-07 16:09:30 +0300

    remove some files

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.listen(port,  () => console.log(`Started ${port}`))
